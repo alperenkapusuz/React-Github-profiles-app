b315f7e0ac04f1ccee98b07c2c04ac6f071c8460
Alperen Kapusuz <63202119+alperenkapusuz@users.noreply.github.com>
2021-09-10 14:47:17 +0300

    repo getirildi

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
-import React from 'react'
+import React, { useState } from "react";
+import axios from "axios";
+import { Form } from "semantic-ui-react";
+import RepositoriesTable from "./components/RepositoriesTable";
+import "./App.css";
 
 const App = () => {
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [repos, setRepos] = useState([]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    searchRepos();
+  }
+
+  function searchRepos() {
+    setLoading(true);
+    axios({
+      method: "get",
+      url: `https://api.github.com/users/${username}/repos`,
+    }).then((res) => {
+      setLoading(false);
+      setRepos(res.data);
+    });
+  }
+
+  function renderRepo(repo) {
+    return <RepositoriesTable repo={repo} />;
+  }
+
   return (
     <div>
-    <h1>sa</h1>
+      <div className="form-div">
+        <Form className="form">
+          <Form.Group>
+            <Form.Input
+              placeholder="username"
+              name="username"
+              className="form-input"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+            />
+            <Form.Button content="Search" onClick={handleSubmit}>
+              {loading ? "Searching..." : "Search"}
+            </Form.Button>
+          </Form.Group>
+        </Form>
+      </div>
+      <div>{repos.map(renderRepo)}</div>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
--- src/components/RepositoriesTable.js
+++ src/components/RepositoriesTable.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React from "react";
+import { List } from "semantic-ui-react";
 
-const RepositoriesTable = () => {
-    return (
-        <div>
-            
-        </div>
-    )
-}
+const RepositoriesTable = ({ repo }) => {
+  return (
+    <div>
+      <List divided relaxed>
+        <List.Item>
+          <List.Icon name="github" size="large" verticalAlign="middle" />
+          <List.Content>
+            <List.Header as="a">{repo.name}</List.Header>
+            <List.Description as="a">Forks: {repo.forks}</List.Description>
+            <List.Description as="a">Langugae: {repo.language}</List.Description>
+            <List.Description as="a">Stars: {repo.stargazers_count}</List.Description>
+          </List.Content>
+          <hr/>
+        </List.Item>
+      </List>
+    </div>
+  );
+};
 
-export default RepositoriesTable
+export default RepositoriesTable;
